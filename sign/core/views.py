f01af635c9a8c527fe195847583a7df08651fe16
lovaraj02 <rajubandaru2255@gmail.com>
2026-04-06 15:29:08 +0530

    params

--- sign/core/views.py
+++ sign/core/views.py
@@ -6,7 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Product
 
-
+# --------------signup-----------------------
 class RegisterApiView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -14,6 +14,18 @@ class RegisterApiView(APIView):
             serializer.save()
             return Response({'msg':'User Registered successfully'})
         return Response(serializer.errors)
+
+# ---------------display login user details---------------
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        user = request.user
+
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        })
     
 
 # ----------------Product view-----------------
@@ -22,5 +34,15 @@ class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
+
+    # ----------params--------------
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        name = self.request.query_params.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
 
     
